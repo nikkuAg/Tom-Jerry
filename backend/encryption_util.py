966691120b5a31a21223d5902d82692f2ca5c784
Divyansh <a.divyansh.25@gmail.com>
2021-10-30 00:07:08 +0530

    Added the encryption and decryption fucntion for the data

---
+++ backend/encryption_util.py
@@ -0,0 +1,29 @@
+from cryptography.fernet import Fernet
+import base64
+import logging
+import traceback
+from UIDAI.config import ENCRYPT_KEY
+
+
+def encrypt(txt):
+    try:
+        txt = str(txt)
+        cipher_suite = Fernet(ENCRYPT_KEY)  # key should be byte
+        encrypted_text = cipher_suite.encrypt(txt.encode('ascii'))
+        encrypted_text = base64.urlsafe_b64encode(
+            encrypted_text).decode("ascii")
+        return encrypted_text
+    except Exception as e:
+        logging.getLogger("error_logger").error(traceback.format_exc())
+        return None
+
+
+def decrypt(txt):
+    try:
+        txt = base64.urlsafe_b64decode(txt)
+        cipher_suite = Fernet(ENCRYPT_KEY)
+        decoded_text = cipher_suite.decrypt(txt).decode("ascii")
+        return decoded_text
+    except Exception as e:
+        logging.getLogger("error_logger").error(traceback.format_exc())
+        return None
